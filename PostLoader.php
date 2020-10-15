5487c4784c792c9ed2bb5917660539e1df73005d
glezzz <alexglezk@gmail.com>
2020-10-15 17:36:58 +0200

    last push

--- PostLoader.php
+++ PostLoader.php
@@ -1,5 +1,6 @@
 <?php
 
+//handles multiple posts at the same time, saves them to a file
 
 class PostLoader
 {
@@ -8,16 +9,24 @@ class PostLoader
 
     public function newPost(string $title, string $content, string $author)
     {
-        array_push($this->posts, $this->newPost($title, $content, $author));  //push post with all its properties tot array
+        /*array_push($this->posts, new Post($title, $content, $author));  //push post with all its properties tot array
+
             if(count($this->posts) > 20){       //if array is higher than 20
                 array_shift($this->posts);  //remove posts past 20
-            }
+            }*/
+    }
+
+    public function printPost()
+    {                                                   //The messages are sorted from new (top) to old (bottom)
+        /*for($i = count($this->posts); $i >= 0; $i--){   //that's why -- instead of ++
+
+            echo $this->posts[$i]->getTitle();
+        }*/
     }
 
     public function loadAllPosts()
     {
         //open a file
-        file_put_contents("", /*data*/);
         //json_decode
         //loop over the data from the file
         //assign each entry to a Post class
--- index.php
+++ index.php
@@ -1,9 +1,66 @@
 <?php
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
 //image this code could be a complex query
 $users = ['John Doe', 'Joe Doe', 'John Smith', 'An Onymous'];
 $articles = ['Terror over london', 'Football: a useless hobby?', 'Economic crisis ahead, says expert', 'Fortis is Fortwas'];
 //end controller
 //start view
+require 'Post.php';
+require 'PostLoader.php';
+session_start();
+
+function whatIsHappening(){
+
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+if(isset($_SESSION['posts'])) {
+    $posts = $_SESSION['posts'];
+
+}else{
+    $posts = new PostLoader();
+}
+
+
+$title = $content = $author = "";
+
+if(isset($_SESSION['title'])){
+    $title = $_SESSION['title'];
+}
+
+if(isset($_SESSION['message'])){
+    $title = $_SESSION['message'];
+}
+
+if(isset($_SESSION['author'])){
+    $title = $_SESSION['author'];
+}
+
+
+
+
+$file = 'posts.json';
+$message = $_POST['content'];
+
+file_put_contents($file, $message);
+
+
+
+whatIsHappening();
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -22,5 +79,8 @@ $articles = ['Terror over london', 'Football: a useless hobby?', 'Economic crisi
         <input type="submit" value="Submit">
 
     </form>
+    <div>
+        <?php $posts->printPosts(); ?>
+    </div>
 </body>
 </html>
